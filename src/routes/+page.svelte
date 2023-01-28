5ba41c083557a9c2d39ad2627eae257cb418cf90
Annuai <annuai.annu@gmail.com>
2023-01-28 17:47:01 +0530

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script type="ts">
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import { onMount,onDestroy } from "svelte"; 
